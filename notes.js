3d80989fa72c2cdb11b2cee725b6c02cd4a08307
mattpettenato <mattpettenato@gmail.com>
2022-06-07 16:59:07 -0700

    depth first search

--- notes.js
+++ notes.js
@@ -55,3 +55,32 @@ function threeNumberSum(array, targetSum){
   return triplets;
 }
 exports.threeNumberSum = threeNumberSum
+
+// depth first search
+
+// Do not edit the class below except
+// for the depthFirstSearch method.
+// Feel free to add new properties
+// and methods to the class.
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+    array.push(this.name);
+    for (const child of this.children) {
+      child.depthFirstSearch(array)
+    }
+    return array;
+  }
+}
+
+// Do not edit the line below.
+exports.Node = Node;
