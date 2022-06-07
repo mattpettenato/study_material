e7c3e31e5819f89e52c3e654cdf6751b80f38369
mattpettenato <mattpettenato@gmail.com>
2022-06-07 16:57:21 -0700

    3 sum

--- notes.js
+++ notes.js
@@ -30,3 +30,28 @@ function twoNumberSum(array, targetSum) {
 
 
 exports.twoNumberSum = twoNumberSum;
+
+// 3sum
+
+function threeNumberSum(array, targetSum){
+  array.sort((a, b) => a - b);
+  const triplets = [];
+  for (let i = 0; i < array.length - 2; i++) {
+    let left = i + 1;
+    let right = array.length - 1;
+    while (left < right) {
+      const currentSum = array[i] + array[left] + array[right];
+      if (currentSum === targetSum) {
+        triplets.push([array[i], array[left], array[right]])
+        left++
+        right--
+      } else if (currentSum < targetSum) {
+        left++
+      } else if (currentSum > targetSum) {
+        right--;
+      }
+    }
+  }
+  return triplets;
+}
+exports.threeNumberSum = threeNumberSum
