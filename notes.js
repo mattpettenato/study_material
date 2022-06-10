2df95aa6234c12b2e826322ec7fce9d97cc5da87
Matt Pettenato <mattpettenato@gmail.com>
2022-06-10 16:57:45 -0700

    'test'

--- notes.js
+++ notes.js
@@ -98,3 +98,20 @@ function getNthFib(n) {
 }
 
 exports.getNthFib = getNthFib;
+
+// Sorted Squared Array
+
+function sortedSquaredArray(array) {
+  const sortedSquares = new Array(array.length).fill(0);
+
+  for (let idx = 0; idx < array.length; idx++) {
+    const value = array[idx]
+    sortedSquares[idx] = value * value;
+  }
+
+  sortedSquares.sort((a, b) => a - b);
+  return sortedSquares;
+}
+
+// Do not edit the line below.
+exports.sortedSquaredArray = sortedSquaredArray;
