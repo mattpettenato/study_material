4d7b96cff7983c1a53f9c65b58f4d75039383206
mattpettenato <mattpettenato@gmail.com>
2022-06-30 16:18:56 -0700

    ajax

---
+++ Data_Structures/javascript.js
@@ -0,0 +1,10 @@
+// Why JavaScript? 
+// - compliment to Java Applets, for little animations
+// - vaguely similar syntax to Java
+// - lightweight and simple: no classes, no integers
+
+// AJAX
+
+// With Ajax, web applications can send and retrieve data from a server asynchronously without interfering with the display and behaviour of the existing page.
+
+// - allows website to communicate with server asynchronously
