94b42c844489819d0c58b48cbb60f6f36ba79a9c
mattpettenato <mattpettenato@gmail.com>
2022-06-29 17:57:26 -0700

    first non repeating character

--- notes.js
+++ notes.js
@@ -117,3 +117,19 @@ function sortedSquaredArray(array) {
 exports.sortedSquaredArray = sortedSquaredArray;
 
 // test
+
+function firstNonRepeatingCharacter(string) {
+  // Write your code here.
+  for (let idx = 0; idx < string.length; idx++) {
+    let foundDuplicate = false;
+    for (let idx2 = 0; idx2 < string.length; idx2++) {
+      if (string[idx] === string[idx2] && idx !== idx2) foundDuplicate = true;
+    }
+
+    if (!foundDuplicate) return idx
+  }
+  return -1;
+}
+
+// Do not edit the line below.
+exports.firstNonRepeatingCharacter = firstNonRepeatingCharacter;
